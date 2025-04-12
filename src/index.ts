60db5ac847547cf24196ca4755ffe902463dd82c
Murat Gözel <murat@gozel.com.tr>
2025-04-12 14:13:55 +0300

    feat: exported resolveSecrets feature

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { pathv, processPathvCalls, hasPathvCalls } from "./pathv/index";
-import { fetchSecretsAws } from "./aws-secret-manager/index";
+import { fetchSecretsAws, resolveReferencesRecursive as resolveSecrets } from "./aws-secret-manager/index";
 
-export { pathv, processPathvCalls, hasPathvCalls, fetchSecretsAws };
+export { pathv, processPathvCalls, hasPathvCalls, fetchSecretsAws, resolveSecrets };
